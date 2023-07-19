2f98961c5b78820f8610fce9eac59a116922152d
groats23 <121118901+groats23@users.noreply.github.com>
2023-07-19 18:16:33 +0200

    summary

---
+++ src/components/AppName/AppName.styled.jsx
@@ -0,0 +1,7 @@
+import styled from '@emotion/styled';
+
+export const AppTitle = styled.h1`
+  margin-bottom: 30px;
+
+  color: brown;
+`;
---
+++ src/components/FeedbackOptions/FeedbackOptions.styled.jsx
@@ -0,0 +1,15 @@
+import styled from '@emotion/styled';
+
+export const FeedbackContainer = styled.div`
+  margin-top: 30px;
+  margin-bottom: 30px;
+`;
+
+export const FeedbackBtn = styled.button`
+  width: 100px;
+  padding: 10px;
+
+  :not(:last-child) {
+    margin-right: 30px;
+  }
+`;
